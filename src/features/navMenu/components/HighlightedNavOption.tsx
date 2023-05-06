e9bae7cd2046bf2042d229d5c1115e7e50d6d524
DESKTOP-0EVV653\ASUS <ezequiel98.08.27@gmail.com>
2023-05-06 11:21:05 -0500

    Adding a new favicon with the proper colors. Adding resume files (language variants) to static public folder, and connecting its paths to the "cv" button in the navBar.

--- src/features/navMenu/components/HighlightedNavOption.tsx
+++ src/features/navMenu/components/HighlightedNavOption.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { resumeText } from '../texts';
 import { useAppSelector } from '../../../redux/hooks';
 import { languageSelectors } from '../../../redux/language';
+import { languageTypes } from '../../../types';
 
 interface IProps {
   navigationLength: number;
@@ -22,31 +23,40 @@ export function HighlightedNavOption({ navigationLength, isMobile }: IProps) {
         delay: 0.8 + 0.15 * navigationLength,
       }}
     >
-      <Button
-        variant="outlined"
-        sx={(theme) => ({
-          fontSize: !isMobile ? '0.82rem' : '1rem',
-          fontWeight: 'bold',
-          backgroundColor: !isMobile
-            ? theme.palette.primary.darker
-            : theme.palette.text.primary,
-          color: theme.palette.background.default,
-          fontStyle: 'normal',
-          borderColor: theme.palette.primary.darker,
-          borderWidth: '2px',
-          borderRadius: '1rem',
-          ':hover': {
-            borderColor: theme.palette.text.primary,
-            borderWidth: '2px',
-            backgroundColor: !isMobile
-              ? theme.palette.text.primary
-              : theme.palette.text.primary,
-          },
-          marginLeft: isMobile ? '1rem' : undefined,
-        })}
+      <a
+        href={
+          language == languageTypes.spanish
+            ? '/ezequiel-cardona-resume-esp.pdf'
+            : '/ezequiel-cardona-resume-eng.pdf'
+        }
+        target="_blank"
       >
-        {resumeText(language)}
-      </Button>
+        <Button
+          variant="outlined"
+          sx={(theme) => ({
+            fontSize: !isMobile ? '0.82rem' : '1rem',
+            fontWeight: 'bold',
+            backgroundColor: !isMobile
+              ? theme.palette.primary.darker
+              : theme.palette.text.primary,
+            color: theme.palette.background.default,
+            fontStyle: 'normal',
+            borderColor: theme.palette.primary.darker,
+            borderWidth: '2px',
+            borderRadius: '1rem',
+            ':hover': {
+              borderColor: theme.palette.text.primary,
+              borderWidth: '2px',
+              backgroundColor: !isMobile
+                ? theme.palette.text.primary
+                : theme.palette.text.primary,
+            },
+            marginLeft: isMobile ? '1rem' : undefined,
+          })}
+        >
+          {resumeText(language)}
+        </Button>
+      </a>
     </motion.div>
   );
 }
